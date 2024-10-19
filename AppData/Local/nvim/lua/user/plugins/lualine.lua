4e4e1fca9bc6adc3351d3776b1f9a2883f50813a
MeShootIn <dmitriimishutin@gmail.com>
2024-10-19 15:54:14 +0500

    Another dotfiles update

--- AppData/Local/nvim/lua/user/plugins/lualine.lua
+++ AppData/Local/nvim/lua/user/plugins/lualine.lua
@@ -5,27 +5,45 @@ if not status_ok then
   return
 end
 
-function session_status()
-  local status = vim.api.nvim_call_function('ObsessionStatus', {'.session.vim', 'SESSION PAUSED OR DELETED!!!'})
+local function session_status()
+  local status = vim.api.nvim_call_function('ObsessionStatus', {'.session.vim', '!!!!!!!!!! SESSION PAUSED OR DELETED !!!!!!!!!!'})
 
   if status == '' then
-    return 'NO SESSION!'
+    return '!!!!! NO SESSION !!!!!'
   end
 
   return status
+end
+
+-- FIXME
+local function session_status_color()
+  local WORKING = 'WORKING'
+  local PAUSED_OR_DELETED = 'PAUSED_OR_DELETED'
+  local NO_SESSION = ''
+  local status = vim.api.nvim_call_function('ObsessionStatus', { WORKING, PAUSED_OR_DELETED })
+
+  if status == WORKING then
+    return { bg = 'red' }
+  elseif status == PAUSED_OR_DELETED then
+    return { bg = 'yellow' }
+  elseif status == NO_SESSION then
+    return { bg = 'grey' }
+  end
 end
 
 lualine.setup {
   options = {
     -- theme = 'solarized_dark',
     -- theme = 'solarized_light',
-    theme = 'onedark',
+    -- theme = 'onedark',
+    theme = 'onelight',
     -- theme = 'tokyonight',
     -- theme = 'dracula',
     -- theme = 'gruvbox_dark',
     -- theme = 'gruvbox_light',
     -- theme = 'gruvbox',
     -- theme = 'gruvbox-material',
+    icons_enabled = true,
     disabled_filetypes = { 'alpha', 'TelescopePrompt', 'NvimTree', 'packer' },
   },
   sections = {
@@ -35,6 +53,11 @@ lualine.setup {
     lualine_c = { 'branch' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = {},
-    lualine_z = { session_status }
+    lualine_z = {
+      {
+        session_status,
+        -- color = session_status_color
+      },
+    }
   },
 }
